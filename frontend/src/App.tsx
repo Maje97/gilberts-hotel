fa690acf77d3fc50f917b5be9a32ae36be46e32d
Alexandra Eriksson <maje@hotmail.se>
2025-03-11 11:08:14 +0100

    Added abou page and images that are going to be used.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router'
 import Layout from './pages/Layout'
 import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Route>
     </Routes>
   )
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,30 @@
+
+export default function About() {
+    return (
+        <main>
+            <h1>About</h1>
+            <p className="text-3xl font-bold underline">About this webpage.</p>
+            <div>
+                <img src="../assets/single.jpg" alt="Image of a hotel room with single bed" />
+                <p>
+                    Image by <a href="https://pixabay.com/users/davidlee770924-1802090/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1330834">ming dai</a> 
+                    from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1330834">Pixabay</a>
+                </p>
+            </div>
+            <div>
+                <img src="../assets/two.jpg" alt="Image of a hotel room with larger size bed" />
+                <p>
+                    Image by <a href="https://pixabay.com/users/peterweideman-18753209/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5664221">Peter Weideman</a> 
+                    from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5664221">Pixabay</a>
+                </p>
+            </div>
+            <div>
+                <img src="../assets/family.jpg" alt="Image of a hotel room with space for a family" />
+                <p>
+                    Image by <a href="https://pixabay.com/users/tianya1223-4833799/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=6878004">THAM YUAN YUAN</a> 
+                    from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=6878004">Pixabay</a>
+                </p> 
+            </div>
+        </main>
+    );
+}
