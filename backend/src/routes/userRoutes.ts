93905b9bbfbbf591fe0ea797b61bf99c6594fb31
Alexandra Eriksson <maje@hotmail.se>
2025-03-21 12:25:31 +0100

    Making routes to register and login, not yet tested. Created a file with http-statuses.

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,26 +1,74 @@
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import prisma from '../prismaClient';
-import { User, UserCredentials } from '../interfaces';
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import prisma from "../prismaClient";
+import dotenv from "dotenv";
+import { User, UserCredentials, CustomJwtPayload } from "../interfaces";
+import { HttpStatus } from "../httpStatus";
 
+dotenv.config();
 const router = express.Router();
 
 //Register new user
-router.post('/register', async (req, res) => {
-    const { username, password } = req.body as UserCredentials
-    const hashedPassword = bcrypt.hashSync(password, 8)
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body as UserCredentials;
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
-    try {
-        const user: User = await prisma.user.create({
-            data: {
-                username,
-                password: hashedPassword
-            }
-        })
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
-        res.json({ token })
-    } catch (err) {}
+  try {
+    const user: User = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+    const token = jwt.sign(
+        { username: user.username, role: user.role } as CustomJwtPayload, 
+        process.env.JWT_SECRET as string, {
+        expiresIn: "1h",
+    });
+    res.status(HttpStatus.CREATED).json({ id: user.id, token: token });
+  } catch (err) {
+    console.log(err);
+    res.status(HttpStatus.SERVICE_UNAVAILABLE);
+  }
 });
 
 //Login user
+router.post("/login", async (req, res) => {
+  const credentials = req.body as UserCredentials;
+
+  try {
+    const storedUser = await prisma.user.findUnique({
+      where: {
+        username: credentials.username,
+      },
+    });
+    if (!storedUser) {
+      res.status(HttpStatus.NOT_FOUND).send({ message: "User not found" });
+      return;
+    }
+
+    const comparePassword = bcrypt.compareSync(
+      credentials.password,
+      storedUser.password
+    );
+    if (!comparePassword) {
+      res
+        .status(HttpStatus.NOT_AUTHORIZED)
+        .send({ message: "Invalid password" });
+      return;
+    }
+
+    const token = jwt.sign(
+      { username: storedUser.username, role: storedUser.role } as CustomJwtPayload,
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" }
+    );
+    res.status(HttpStatus.OK).json({ token });
+  } catch (err) {
+    console.log(err);
+    res.status(HttpStatus.SERVICE_UNAVAILABLE);
+  }
+});
+
+export default router;
