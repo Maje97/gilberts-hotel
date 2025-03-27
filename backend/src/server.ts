99e2b779095de41da6727011331ae9391126eec7
Alexandra Eriksson <maje@hotmail.se>
2025-03-27 14:37:21 +0100

    Trying and failing docker. Something wrong with credentials. Updating prisma.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,16 @@
 import express from 'express';
+import userRoutes from './routes/userRoutes';
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
+
+//Middlewares
+
+//Routes
+app.use('/user', userRoutes)
+//app.use('/room', roomRoutes)
+//app.use('/booking', bookingRoutes)
+
+app.listen(port, () => {
+    console.log(`Server has started on port: ${port}`)
+})
