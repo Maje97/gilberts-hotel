a212aee6fb75c39d127bc175456972b6509de779
Alexandra Eriksson <maje@hotmail.se>
2025-03-28 09:37:58 +0100

    Lagt till res sidan saknas för localhost:8080, lagt till dotenv.config();

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,24 @@
-import express from 'express';
+import express, {Express} from 'express';
+import dotenv from "dotenv";
 import userRoutes from './routes/userRoutes';
+import { HttpStatus } from './httpStatus';
 
-const app = express();
+dotenv.config();
+
+const app: Express = express();
 const port = process.env.PORT || 8080;
 
 //Middlewares
-
+app.use(express.json());
 //Routes
-app.use('/user', userRoutes)
+app.use('/user', userRoutes);
 //app.use('/room', roomRoutes)
 //app.use('/booking', bookingRoutes)
 
+app.use((req, res) => {
+    res.status(HttpStatus.NOT_FOUND).send('Sidan saknas.');
+});
+
 app.listen(port, () => {
-    console.log(`Server has started on port: ${port}`)
-})
+    console.log(`Server has started on port: ${port}`);
+});
