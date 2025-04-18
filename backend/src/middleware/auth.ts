06a210c32a0544be5338717fd888d2b217c5f263
Alexandra Eriksson <maje@hotmail.se>
2025-04-18 15:21:08 +0200

    Missed to include return when admin in middleware. Included it now. Now verifies jwt in try/catch.

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -18,29 +18,35 @@ export function auth(requiredPermissions: Permission[]) {
     return (req: Request, res: Response, next: NextFunction): void => {
         const token = req.headers['authorization'];
         if (!token) { 
-            res.status(HttpStatus.NOT_AUTHENTICATED).send('No token was recieved.');
-            return
+            void res.status(HttpStatus.NOT_AUTHENTICATED).send('No token was recieved.');
+            return;
         }
-            
-        const payload = jwt.verify(token, secret) as CustomJwtPayload;
-        if (!payload) {
-            res.status(HttpStatus.NOT_AUTHENTICATED).send('Token is not valid')
-            return
+        
+        let payload: CustomJwtPayload;
+        try {
+            payload = jwt.verify(token, secret) as CustomJwtPayload;
+        } catch (err) {
+            void res.status(HttpStatus.NOT_AUTHENTICATED).send('Token is not valid');
+            return; 
         }
         
         const RolePermissions: RolesWithPermissions = {
             [Role.ADMIN]: ['create', 'read', 'update', 'delete'],
             [Role.USER]: ['read'],
         };
-        const {role} = payload as CustomJwtPayload
+
+        const { role } = payload;
+        
         const userPermissions = RolePermissions[role];
 
         // Skip authorization if admin.
-        if (role === Role.ADMIN) next();
+        if (role === Role.ADMIN) {
+            return next();
+        }
 
         //If user permissions are missing
         if (!userPermissions) {
-            res.status(HttpStatus.NOT_AUTHORIZED).send('User is not authorized.')
+            void res.status(HttpStatus.NOT_AUTHORIZED).send('User is not authorized.')
             return
         }
 
@@ -51,7 +57,7 @@ export function auth(requiredPermissions: Permission[]) {
 
         //Decline user if not authorized, otherwise continue to next.
         if (!hasRequiredPermissions) {
-            res.status(HttpStatus.NOT_AUTHORIZED).send('User is not authorized, access declined.');
+            void res.status(HttpStatus.NOT_AUTHORIZED).send('User is not authorized, access declined.');
             return
         }
 
